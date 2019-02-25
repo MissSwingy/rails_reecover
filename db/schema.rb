0c9df0b8c67c8e926bcfd0300d2f6fbec0e46c2b
Natalia Vega <nvegar@gmail.com>
2019-02-25 15:24:36 +0100

    model answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_141843) do
+ActiveRecord::Schema.define(version: 2019_02_25_142305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "column"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.bigint "survey_id"
@@ -45,5 +53,6 @@ ActiveRecord::Schema.define(version: 2019_02_25_141843) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "surveys"
 end
