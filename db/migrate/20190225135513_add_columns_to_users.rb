ce999824f123a34b662e06b183cb291c76a088a1
Francois Nguyen <francois.nguyen2@gmail.com>
2019-02-25 14:59:56 +0100

    added User model withouth photo column

---
+++ db/migrate/20190225135513_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :sport, :string
+    add_column :users, :bio, :string
+    add_column :users, :address, :string
+  end
+end
