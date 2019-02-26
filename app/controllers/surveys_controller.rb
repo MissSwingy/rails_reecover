6f6d3400b2b08cdb48e7ade4ac202d341de1468f
Natalia Vega <nvegar@gmail.com>
2019-02-26 13:08:08 +0100

    first mock question test

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,8 +1,11 @@
 class SurveysController < ApplicationController
   def index
+    @surveys = Survey.all
   end
 
   def show
-    # @survey = Survey.find(params[:id])
+    @survey = Survey.find(params[:id])
+    @question = Question.find(@survey.id)
+    @answer = Answer.find(@question.id)
   end
 end
