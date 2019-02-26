f88bf75239e24cfbe9f370432fba0edaa11a0af7
Francois Nguyen <francois.nguyen2@gmail.com>
2019-02-26 18:08:12 +0100

    added markers in user controller and geocoder initializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @training_centers = current_user.user_personality.career.training_center.where.not(latitude: nil, longitude: nil)
+
+    @markers = @training_centers.map do |training_center|
+      {
+        lng: training_center.longitude,
+        lat: training_center.latitude
+      }
+
   end
 end
