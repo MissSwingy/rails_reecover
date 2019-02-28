0fe90bdfae855ca67afb802cfa4c0741654e3687
Natalia Vega <nvegar@gmail.com>
2019-02-28 19:18:12 +0100

    seeds modified controller show metiers+personalité results test

--- app/models/users_personality.rb
+++ app/models/users_personality.rb
@@ -1,5 +1,5 @@
 class UsersPersonality < ApplicationRecord
   belongs_to :user
-  belongs_to :career
-  belongs_to :personalityriasec
+  belongs_to :career, optional: true
+  belongs_to :personalityriasec, optional: true
 end
