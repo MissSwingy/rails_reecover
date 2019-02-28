7816604a13de9d4fc920c14757abec756ac74739
Francois Nguyen <francois.nguyen2@gmail.com>
2019-02-28 13:19:17 +0100

    modified routes and add create edit and update method for attempt controller

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,21 +1,26 @@
 class AttemptsController < ApplicationController
+
   def create
-    @attempt = Attempt.new
+    @survey = Survey.find(params[:id])
+    @attempt = Attempt.create(user: current_user, survey: @survey, result: "")
+    redirect_to edit_attempt_path(@attempt)
+  end
+
+  def edit
+    @attempt = Attempt.find(params[:id])
+    @user = current_user
   end
 
   def update
-
-  end
-
-  def algo_result
+    @attempt = Attempt.find(params[:id])
     @user = current_user
-    @user_answers = UserAnswer.all.where(user_id: @user)
-    @attempt = Attempt.where(user_id: @user).last
+    @user_answers = UserAnswer.all.where(user_id: @user) #ne marchera pas
+    # @attempt = Attempt.where(user_id: @user).last
     @user_answers.each do |answer|
       @attempt.result << answer.answer.column
     end
     calcul = @attempt.result.split('')
     result = calcul.max
     @attempt.result = result
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :users_answers, only: [:create]
   end
   resources :surveys do
-    resources :attempts, only: [:new, :create]
+    resources :attempts, only: [:edit, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
