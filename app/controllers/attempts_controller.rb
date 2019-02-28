5926d62f02d0b2a6e51f1b804fe7046f43455f55
Francois Nguyen <francois.nguyen2@gmail.com>
2019-02-28 11:39:14 +0100

    added a method for algo result

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,3 +1,18 @@
 class AttemptsController < ApplicationController
+  def create
 
+  end
+
+  def update
+
+  end
+
+  def algo_result
+    @user = current_user
+    @user_answers = UserAnswer.all.where(user_id: @user)
+    @attempt = Attempt.last
+    @attempt.result = []
+    @user_answers.each do |answer|
+
+  end
 end
