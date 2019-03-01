e8ce10460f7d1be8336f2ea9b4c81c8c33adc378
Natalia Vega <nvegar@gmail.com>
2019-03-01 13:25:36 +0100

    modif path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    dashboard_path(current_user)
+    root_path(current_user)
   end
 
   def default_url_options
