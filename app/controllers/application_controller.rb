245fe0e769641dc97bb330720943bbe958d37cf6
Natalia Vega <nvegar@gmail.com>
2019-03-03 22:47:02 +0100

    Traduction i18n forms, 3 gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     root_path(current_user)
@@ -9,4 +10,13 @@ class ApplicationController < ActionController::Base
   def default_url_options
   { host: ENV["HOST"] || "localhost:3000" }
   end
+
+  def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
 end
