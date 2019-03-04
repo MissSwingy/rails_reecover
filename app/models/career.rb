1d7ad10143d50414dd1aa8331f8ad22f31a57751
Natalia Vega <nvegar@gmail.com>
2019-03-04 23:21:08 +0100

    pg search works and gets a ActiveRecord::Relation

--- app/models/career.rb
+++ app/models/career.rb
@@ -2,9 +2,9 @@ class Career < ApplicationRecord
   belongs_to :personality_riasec
   has_many :training_centers
   has_many :users_personalities
-
   include PgSearch
   multisearchable against: [:title, :category]
+  scope :find_title, lambda { |search| where("title ILIKE :search", search: "%#{search}%") }
 
   # def initialize
   #   PgSearch.multisearch_options = {
