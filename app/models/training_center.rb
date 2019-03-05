b1246cfe1e1f2d2bc86c47d6e7ca2bc6cd2c4bcd
Natalia Vega <nvegar@gmail.com>
2019-03-05 12:57:10 +0100

    Dashboard + new user modif

--- app/models/training_center.rb
+++ app/models/training_center.rb
@@ -1,6 +1,6 @@
 class TrainingCenter < ApplicationRecord
   belongs_to :career
   include PgSearch
-  multisearchable against: [:name, :city]
+  multisearchable against: [:name, :city, :category]
   scope :find_name, lambda { |search| where("name ILIKE :search", search: "%#{search}%") }
 end
