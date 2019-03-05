f1ee9e2fbc5cf89343c8842142619eb4e14b154c
Francois Nguyen <francois.nguyen2@gmail.com>
2019-03-05 12:37:37 +0100

    added map on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,14 +2,18 @@ class DashboardsController < ApplicationController
   def show
     @user = current_user
     @user_personality = UsersPersonality.find_by(user_id: current_user)
+    @training_centers = TrainingCenter.where(career_id: @user_personality[:career_id]).where.not(latitude: nil, longitude: nil).near(@user, 1000)
 
-    @training_centers = @user_personality.careers.training_centers.where.not(latitude: nil, longitude: nil)
-
-    # @markers = @training_centers.map do |training_center|
-    #   {
-    #     lng: training_center.longitude,
-    #     lat: training_center.latitude
-    #   }
-    # end
+    @markers = @training_centers.first(3).map do |training_center|
+      {
+        lng: training_center.longitude,
+        lat: training_center.latitude
+      }
+    end
+    @markers << {
+      lng: @user.longitude,
+      lat: @user.latitude,
+      image_url: helpers.asset_url(@user.photo)
+    }
   end
 end
